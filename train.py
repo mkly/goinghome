99cd83a3aeacbfdfb9f76559256296ea1c14b53f
Mike Lay <mike@mkly.io>
2026-07-21 11:53:27 -0700

    Update train.py to save model as xgb_live_model.json

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pickle
 from xgboost import XGBRegressor
 
 # 1. Load the clean data pickle we made in the ETL step
@@ -14,8 +13,8 @@ print("Training model...")
 model = XGBRegressor(n_estimators=300, learning_rate=0.05, max_depth=6)
 model.fit(X, y)
 
-# 4. SAVE THE MODEL FOR THE CLI SCRIPT
-with open("xgb_live_model.pkl", "wb") as f:
-    pickle.dump(model, f)
+# 4. SAVE THE MODEL IN NATIVE JSON FORMAT
+model.save_model("xgb_live_model.json")
 
-print("SUCCESS: Model trained and saved as 'xgb_live_model.pkl'!")
+print("SUCCESS: Model trained and saved as 'xgb_live_model.json'!")
+
